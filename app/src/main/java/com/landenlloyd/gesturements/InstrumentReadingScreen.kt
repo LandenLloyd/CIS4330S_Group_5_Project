7e1a6fec9d72786f0b5d324b2e5769b906ca1c78
Landen Lloyd <oldestoflloyd@gmail.com>
2023-03-30 10:25:06 -0400

    Attempt at building a frame system into Sensor3DViewModel.

--- app/src/main/java/com/landenlloyd/gesturements/InstrumentReadingScreen.kt
+++ app/src/main/java/com/landenlloyd/gesturements/InstrumentReadingScreen.kt
@@ -10,13 +10,90 @@ import androidx.compose.ui.res.stringResource
 import androidx.lifecycle.ViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
+import kotlin.math.roundToInt
 
-class Sensor3DViewModel : ViewModel() {
+class Sensor3DViewModel(frameWidth: Int = 20, private val overlap: Float = 0f) :
+    ViewModel() {
+    private val _frame = SensorFrame(frameWidth)
+
     private val _sensorState = MutableStateFlow(SensorState())
     val sensorState = _sensorState.asStateFlow()
 
     fun updateReadings(x: Float, y: Float, z: Float) {
-        _sensorState.value = SensorState(x, y, z)
+        if (_frame.updateReadings(x, y, z)) {
+            val (_x, _y, _z) = _frame.getAverages()
+            _sensorState.value = SensorState(_x, _y, _z)
+            _frame.clear(overlap)
+        }
+    }
+}
+
+class SensorFrame(private val frameWidth: Int) {
+    private var _content = SensorFrameContent(frameWidth)
+    private var _frameSize = 0
+
+    // Returns true if the frame is at capacity after adding the readings.
+    // Does not add any new elements if the frame is already at capacity
+    fun updateReadings(x: Float, y: Float, z: Float): Boolean {
+        if (_frameSize == frameWidth) {
+            return true
+        }
+
+        _content.x[_frameSize] = x
+        _content.y[_frameSize] = y
+        _content.z[_frameSize] = z
+        _frameSize += 1
+
+        return _frameSize == frameWidth
+    }
+
+    // Removes most elements, but keeps `overlap` proportion of elements
+    fun clear(overlap: Float) {
+        val numOverlap = (frameWidth * overlap).roundToInt()
+
+        val overlapElementsX = _content.x.takeLast(numOverlap).toFloatArray().copyOf(frameWidth)
+        val overlapElementsY = _content.y.takeLast(numOverlap).toFloatArray().copyOf(frameWidth)
+        val overlapElementsZ = _content.z.takeLast(numOverlap).toFloatArray().copyOf(frameWidth)
+
+        _content =
+            SensorFrameContent(frameWidth, overlapElementsX, overlapElementsY, overlapElementsZ)
+        _frameSize = numOverlap
+    }
+
+    fun getAverages(): Triple<Float, Float, Float> {
+        val x = _content.x.average().toFloat()
+        val y = _content.y.average().toFloat()
+        val z = _content.z.average().toFloat()
+        return Triple(x, y, z)
+    }
+}
+
+data class SensorFrameContent(
+    val frameWidth: Int,
+    val x: FloatArray = FloatArray(frameWidth) { 0f },
+    val y: FloatArray = FloatArray(frameWidth) { 0f },
+    val z: FloatArray = FloatArray(frameWidth) { 0f },
+) {
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (javaClass != other?.javaClass) return false
+
+        other as SensorFrameContent
+
+        if (frameWidth != other.frameWidth) return false
+        if (!x.contentEquals(other.x)) return false
+        if (!y.contentEquals(other.y)) return false
+        if (!z.contentEquals(other.z)) return false
+
+        return true
+    }
+
+    override fun hashCode(): Int {
+        var result = frameWidth
+        result = 31 * result + x.contentHashCode()
+        result = 31 * result + y.contentHashCode()
+        result = 31 * result + z.contentHashCode()
+        return result
     }
 }
 
