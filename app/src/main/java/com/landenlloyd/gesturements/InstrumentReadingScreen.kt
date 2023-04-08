f83c46dac27098f6e78a6712b6bd477cf41837fd
Landen Lloyd <oldestoflloyd@gmail.com>
2023-04-07 21:39:25 -0400

    Interpolate time frames.

--- app/src/main/java/com/landenlloyd/gesturements/InstrumentReadingScreen.kt
+++ app/src/main/java/com/landenlloyd/gesturements/InstrumentReadingScreen.kt
@@ -10,7 +10,10 @@ import androidx.compose.ui.res.stringResource
 import androidx.lifecycle.ViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
+import org.apache.commons.math4.legacy.analysis.UnivariateFunction
+import org.apache.commons.math4.legacy.analysis.interpolation.SplineInterpolator
 import kotlin.math.roundToInt
+import org.apache.commons.math4.legacy.analysis.interpolation.UnivariateInterpolator
 
 class Sensor3DViewModel(frameWidth: Int = 20, private val overlap: Float = 0f) :
     ViewModel() {
@@ -20,7 +23,7 @@ class Sensor3DViewModel(frameWidth: Int = 20, private val overlap: Float = 0f) :
     val sensorState = _sensorState.asStateFlow()
 
     fun updateReadings(t: Long, x: Float, y: Float, z: Float) {
-        if (_frame.updateReadings(t, x, y, z)) {
+        if (_frame.updateReadings(t.toDouble(), x.toDouble(), y.toDouble(), z.toDouble())) {
             val (_x, _y, _z) = _frame.getAverages()
             _sensorState.value = SensorState(_x, _y, _z)
             _frame.clear(overlap)
@@ -34,7 +37,7 @@ class SensorFrame(private val frameWidth: Int) {
 
     // Returns true if the frame is at capacity after adding the readings.
     // Does not add any new elements if the frame is already at capacity
-    fun updateReadings(t: Long, x: Float, y: Float, z: Float): Boolean {
+    fun updateReadings(t: Double, x: Double, y: Double, z: Double): Boolean {
         if (_frameSize == frameWidth) {
             return true
         }
@@ -52,10 +55,10 @@ class SensorFrame(private val frameWidth: Int) {
     fun clear(overlap: Float) {
         val numOverlap = (frameWidth * overlap).roundToInt()
 
-        val overlapElementsT = _content.t.takeLast(numOverlap).toLongArray().copyOf(frameWidth)
-        val overlapElementsX = _content.x.takeLast(numOverlap).toFloatArray().copyOf(frameWidth)
-        val overlapElementsY = _content.y.takeLast(numOverlap).toFloatArray().copyOf(frameWidth)
-        val overlapElementsZ = _content.z.takeLast(numOverlap).toFloatArray().copyOf(frameWidth)
+        val overlapElementsT = _content.t.takeLast(numOverlap).toDoubleArray().copyOf(frameWidth)
+        val overlapElementsX = _content.x.takeLast(numOverlap).toDoubleArray().copyOf(frameWidth)
+        val overlapElementsY = _content.y.takeLast(numOverlap).toDoubleArray().copyOf(frameWidth)
+        val overlapElementsZ = _content.z.takeLast(numOverlap).toDoubleArray().copyOf(frameWidth)
 
         _content =
             SensorFrameContent(
@@ -78,10 +81,10 @@ class SensorFrame(private val frameWidth: Int) {
 
 data class SensorFrameContent(
     val frameWidth: Int,
-    val t: LongArray = LongArray(frameWidth) { 0 },
-    val x: FloatArray = FloatArray(frameWidth) { 0f },
-    val y: FloatArray = FloatArray(frameWidth) { 0f },
-    val z: FloatArray = FloatArray(frameWidth) { 0f },
+    val t: DoubleArray = DoubleArray(frameWidth) { 0.0 },
+    val x: DoubleArray = DoubleArray(frameWidth) { 0.0 },
+    val y: DoubleArray = DoubleArray(frameWidth) { 0.0 },
+    val z: DoubleArray = DoubleArray(frameWidth) { 0.0 },
 ) {
     override fun equals(other: Any?): Boolean {
         if (this === other) return true
@@ -113,6 +116,30 @@ data class SensorState(
     val y: Float = 0f,
     val z: Float = 0f
 )
+
+// Syncs the content in x2, y2, and z2 to the time t1.
+// Returns the new x2, y2, and z2
+fun syncFrames(
+    t1: DoubleArray,
+    x1: DoubleArray,
+    y1: DoubleArray,
+    z1: DoubleArray,
+    t2: DoubleArray,
+    x2: DoubleArray,
+    y2: DoubleArray,
+    z2: DoubleArray
+): Triple<DoubleArray, DoubleArray, DoubleArray> {
+    val interpolator: UnivariateInterpolator = SplineInterpolator()
+    val functionX: UnivariateFunction = interpolator.interpolate(t2, x2)
+    val functionY: UnivariateFunction = interpolator.interpolate(t2, y2)
+    val functionZ: UnivariateFunction = interpolator.interpolate(t2, z2)
+
+    val newX1 = DoubleArray(t1.size) { functionX.value(t1[it]) }
+    val newY1 = DoubleArray(t1.size) { functionY.value(t1[it]) }
+    val newZ1 = DoubleArray(t1.size) { functionZ.value(t1[it]) }
+
+    return Triple(newX1, newY1, newZ1)
+}
 
 @Composable
 fun AccelerometerReading(
